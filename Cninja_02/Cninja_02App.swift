8593e6f5c21c3ebdad267757f767f0f32b31e3ca
Sky1433 <Samaanawaf@gmail.com>
2023-11-27 09:35:40 +0300

    Initial Commit

---
+++ Cninja_02/Cninja_02App.swift
@@ -0,0 +1,17 @@
+//
+//  Cninja_02App.swift
+//  Cninja_02
+//
+//  Created by Samaa on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cninja_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cninja_02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cninja_02
+//
+//  Created by Samaa on 27/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
